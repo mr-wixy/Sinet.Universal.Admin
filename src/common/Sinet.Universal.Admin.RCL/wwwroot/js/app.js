e3aff227bcc651da31002e67496597d59d1ff73d
wangyu <wixy@qq.com>
2023-11-15 09:36:28 +0800

    添加Loading界面

---
+++ src/common/Sinet.Universal.Admin.RCL/wwwroot/js/app.js
@@ -0,0 +1,9 @@
+﻿window.app = {
+    loadingStart: () => {
+        document.getElementById("app").classList.add("loader")
+    },
+    loadingStop: () => {
+        document.getElementById("app").classList.remove("loader")
+        document.body.classList.remove('preloadbody');
+    }
+}
